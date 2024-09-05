4f0147c47d2bccbfa3db8eb403917e1898d58fe6
Matheus Gaspar <msouzagaspar@gmail.com>
2024-09-05 13:47:38 -0300

    adiciona autenticação

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2024_09_03_191023) do
+ActiveRecord::Schema[8.0].define(version: 2024_09_04_195413) do
   create_table "registros", force: :cascade do |t|
     t.string "nome"
     t.text "descricao"
@@ -20,4 +20,23 @@ ActiveRecord::Schema[8.0].define(version: 2024_09_03_191023) do
     t.decimal "valor"
     t.boolean "entrada"
   end
+
+  create_table "sessions", force: :cascade do |t|
+    t.integer "user_id", null: false
+    t.string "ip_address"
+    t.string "user_agent"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_sessions_on_user_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "email_address", null: false
+    t.string "password_digest", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email_address"], name: "index_users_on_email_address", unique: true
+  end
+
+  add_foreign_key "sessions", "users"
 end
