34ce7450ae05caaf04a5e3432c9fb23f80cefc1b
Matheus Gaspar <msouzagaspar@gmail.com>
2024-09-11 18:03:20 -0300

    adiciona edicao de registros

--- app/controllers/registros_controller.rb
+++ app/controllers/registros_controller.rb
@@ -4,4 +4,19 @@ class RegistrosController < ApplicationController
     @registros = Registro.all
   end
 
+  def show
+    @registro = Registro.find(params[:id])
+  end
+  
+  def edit
+    @registro = Registro.find(params[:id])
+  end
+
+  def update
+    @registro = Registro.find(params[:id])
+    registro_params = params.require(:registro).permit(:nome, :valor, :data)
+    @registro.update(registro_params)
+    redirect_to registros_path
+  end
+
 end
